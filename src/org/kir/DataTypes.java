8db697454be4bfa9c63d1d91077ee498da1547db
Balaji Ravichandran <balajicrvino@gamil.com>
2024-06-16 12:32:48 +0530

    Datatypes completed

---
+++ src/org/kir/DataTypes.java
@@ -0,0 +1,36 @@
+package org.kir;
+
+public class DataTypes {
+
+	public static void main(String[] args) {
+
+		byte b = -128;
+
+		int i = 100;
+
+		long l = 9095702844l;
+
+		char c = 'd';
+
+		String s = "Test Program123";
+
+		boolean bool = true;
+
+		float f = 123.4567f;
+
+		System.out.println(b);
+
+		System.out.println(i);
+
+		System.out.println(l);
+
+		System.out.println(c);
+
+		System.out.println(s);
+
+		System.out.println(bool);
+
+		System.out.println(f);
+	}
+
+}
--- src/org/kir/TestProgram.java
+++ src/org/kir/TestProgram.java
@@ -2,7 +2,30 @@ package org.kir;
 
 public class TestProgram {
 
+	private void methodA() {
+		System.out.println("This is MethodA");
+
+	}
+
+	private void methodB() {
+		System.out.println("This is MethodB");
+	}
+
+	private void methodC() {
+		System.out.println("This is MethodC");
+	}
+
+	public void methodName() {
+
+	}
+
 	public static void main(String[] args) {
+
+		TestProgram obj = new TestProgram();
+
+		obj.methodA();
+		obj.methodB();
+		obj.methodC();
 
 		System.out.println("Hello World !");
 	}
